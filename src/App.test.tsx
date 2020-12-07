6dfd6be8b17dce55d2f73eba6eb1bf25c9847748
Navin <navin007.a@gmail.com>
2020-12-07 14:00:17 +0530

    feat(storybook): ✨  add storybook & add button components

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders hello world", () => {
-  render(<App />);
-  const h1Element = screen.getByText(/Hello World/i);
-  expect(h1Element).toBeInTheDocument();
-});

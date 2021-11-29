24ef470cbf495b81b08db9a7a34e8e33dd6b2bd8
Navin Moorthy <navin007.a@gmail.com>
2021-11-29 12:23:42 +0530

    docs(storybook): 📝 update stories to csf 3 format

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,7 @@
 module.exports = {
+  framework: "@storybook/react",
+  core: { builder: "webpack5" },
+  features: { storyStoreV7: true, babelModeV7: true },
   stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
 };
